92732ff3d2835dca97587c11df7e57bfb2a500bd
Nima Gharibloo <nima.gharibloo@gmail.com>
2023-07-21 16:45:00 +0330

    a better displaying format for the assembler

--- Assembler/assembler.py
+++ Assembler/assembler.py
@@ -2,10 +2,10 @@ import sys
 
 opcodes = {'JMP':'0', 'ADC':'1', 'XOR':'2', 'SBC':'3', 'ROR':'4', 'TAT':'5', 'OR':'6', 'AND':'8',
            'LDC':'9', 'BCC':'a', 'BNE':'b', 'LDI':'c', 'STT':'d', 'LDA':'e', 'STA':'f' }
-immidiateNeededInstructions = ('JMP', 'ADC', 'XOR', 'SBC', 'OR', 'AND', 'LDC', 'BCC', 'BNE', 'LDA', 'STA')
+immidiate_Needed_Instructions = ('JMP', 'ADC', 'XOR', 'SBC', 'OR', 'AND', 'LDC', 'BCC', 'BNE', 'LDA', 'STA')
 jumps = ('JMP', 'BCC', 'BNE')
 
-instrcutions, insAddresses, variables, labels = [], [], {}, {}
+instructions, insAddresses, variables, labels = [], [], {}, {}
 lineAddress = 1
 
 if len(sys.argv) != 2 : 
@@ -33,18 +33,20 @@ while True :
             labels[label] = "{:03x}".format(lineAddress)
             line = line[endOfLabel+1 : ]
         if line.strip():
-            instrcutions.append(line.strip())
+            instructions.append(line.strip())
             insAddresses.append("{:03x}".format(lineAddress))
             lineAddress += 1
 file.close()
 
-print(instrcutions)
-print(insAddresses)
-print(variables)
-print(labels)
+print(f'instructions: {instructions}')
+print(f'ins addresses: {insAddresses}')
+print(f'variables: {variables}')
+print(f'labels: {labels}')
 
-for i in range(0 , len(instrcutions)) :
-    ins = instrcutions[i].split()
+
+for i in range(0 , len(instructions)) :
+    instrcution = instructions[i]
+    ins = instrcution.split()
     ins[0] = ins[0].upper()
 
     if ins[0] in opcodes.keys() :
@@ -53,10 +55,10 @@ for i in range(0 , len(instrcutions)) :
         print('an unknown instruction has been used!')
         sys.exit(1)
 
-    if len(ins) == 2 and ins[0] not in immidiateNeededInstructions :
+    if len(ins) == 2 and ins[0] not in immidiate_Needed_Instructions :
         print(f"instruction {ins[0]} doesn't accept any value!")
         sys.exit(1)
-    elif len(ins) == 1 and ins[0] in immidiateNeededInstructions :
+    elif len(ins) == 1 and ins[0] in immidiate_Needed_Instructions :
         print(f"instruction {ins[0]} needs an immidiate value!")
         sys.exit(1)
 
@@ -71,15 +73,16 @@ for i in range(0 , len(instrcutions)) :
         else :
             hexNum = ins[1][2:] if(ins[1].find('0x') != -1) else "{:03x}".format( int(ins[1], 10) )
         
-        instrcutions[i] = f'{insAddresses[i]} : {opcode} {hexNum}'
+        instructions[i] = f'{insAddresses[i]} : {opcode} {hexNum} \t\t {instrcution}'
     
     elif len(ins) == 1 :
-        instrcutions[i] = f'{insAddresses[i]} : {opcode}'
+        instructions[i] = f'{insAddresses[i]} : {opcode} \t\t {instrcution}'
     
     else :
         print(f"instruction {ins[0]} hasn't been used correctly!")
         exit(1)
 
+
 print()
-for ins in instrcutions :
+for ins in instructions :
     print(ins)
